1cf1e39b94aaf85f85cd84a13cfbdc5cd9f99eff
BOBBY <BOBBYKWONGZW@GMAIL.COM>
2020-06-09 15:13:07 +0800

    playing around with graphql schema

--- schema/schema.js
+++ schema/schema.js
@@ -1,13 +1,57 @@
 const graphql = require('graphql')
+const _ = require('lodash')
 
-const {GraphQLObjectType, GraphQLString} = graphql;
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLSchema,
+    GraphQLID,
+    GraphQLList
+} = graphql;
+
+// Dummy data
+const songs = [
+    {title: 'wakada', album_cover_url: 'www.wakanda', id: '1', artist_id: '1'},
+    {title: 'hello', album_cover_url: 'www.hello', id: '2', artist_id: '2'},
+    {title: 'hold on', album_cover_url: 'www.holdon', id: '2', artist_id: '2'},
+    {title: 'later', album_cover_url: 'www.later', id: '2', artist_id: '2'},
+    {title: 'lol', album_cover_url: 'www.lollll', id: '3', artist_id: '3'},
+    {title: 'zzzz', album_cover_url: 'www.zzzzz', id: '3', artist_id: '3'}
+]
+
+const artists = [
+    {name: 'bobby', id: '1'},
+    {name: 'sam', id: '2'},
+    {name: 'paul', id: '3'}
+]
 
 const SongType = new GraphQLObjectType({
     name: 'Song',
     fields: () => ({
-        id: {type: GraphQLString},
+        id: {type: GraphQLID},
         title: {type: GraphQLString},
-        album_cover_url: {type: GraphQLString}
+        album_cover_url: {type: GraphQLString},
+        artist: {
+            type: ArtistType,
+            resolve(parent, args){
+                console.log(parent);
+                return _.find(artists, {id: parent.artist_id});
+            }
+        }
+    })
+});
+
+const ArtistType = new GraphQLObjectType({
+    name: 'Artist',
+    fields: () => ({
+        id: {type: GraphQLID},
+        name: {type: GraphQLString},
+        songs: {
+            type: new GraphQLList(SongType),
+            resolve(parent, args){
+                return _.filter(songs, {artist_id: parent.id});
+            }
+        }
     })
 });
 
@@ -16,7 +60,36 @@ const RootQuery = new GraphQLObjectType({
     fields: {
         song: {
             type: SongType,
-            args: {id: {type: GraphQLString}}
+            args: {id: {type: GraphQLID}},
+            resolve(parent, args){
+                // code to get data from db
+                console.log(typeof(args.id))
+                return _.find(songs, {id: args.id});
+            }
+        },
+        artist: {
+            type: ArtistType,
+            args: {id: {type: GraphQLID}},
+            resolve(parent, args){
+                // code to get data from db
+                return _.find(artists, {id: args.id});
+            }
+        },
+        songs: {
+            type: new GraphQLList(SongType),
+            resolve(parent, args){
+                return songs
+            }
+        },
+        artists: {
+            type: new GraphQLList(ArtistType),
+            resolve(parent, args){
+                return artists
+            }
         }
     }
+})
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
 })
