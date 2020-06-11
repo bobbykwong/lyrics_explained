e40d77063f3388046b840a0349ef54cbf5c1cc62
BOBBY <BOBBYKWONGZW@GMAIL.COM>
2020-06-11 19:06:10 +0800

    able to insert data from client to database

--- client/src/components/addlyrics/Form.jsx
+++ client/src/components/addlyrics/Form.jsx
@@ -1,22 +1,42 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { useMutation } from "@apollo/react-hooks"
+import {gql} from 'apollo-boost';
+
+const ADD_ARTIST = gql`
+    mutation($name: String!, $artist_cover: String!) {
+        addArtist(name: $name, artist_cover: $artist_cover){
+            name
+        }
+    }
+`;
 
 function Form() {
     const [clicked, setClick] = useState([0]);
-    console.log(clicked)
+    const [artistName, setArtistName] = useState("");
+    const [artistCover, setArtistCover] = useState("");
 
-    const verseField = clicked.map(field => {
+    let input;
+    const [addArtist, { data }] = useMutation(ADD_ARTIST);
+
+    const verseField = clicked.map((field, index) => {
         return (
-            <textarea className="form-control input-field" placeholder="Lyrics here. Add a new verse whenever you think it deserves its own set of interpretations"/>
+            <textarea key={index} className="form-control input-field" placeholder="Lyrics here. Add a new verse whenever you think it deserves its own set of interpretations"/>
         )
     })
 
+    const submitHandler = (e) => {
+        e.preventDefault();
+        addArtist({ variables: { name: artistName, artist_cover: artistCover } });
+    }
+
     return(
-        <form className="lyrics-form text-center">
-            <input className="form-control input-field" placeholder="Artist"/>
-            <input className="form-control input-field" placeholder="Artist Cover Image link"/>
+        <form className="lyrics-form text-center" onSubmit={e => {submitHandler(e)}}>
+            <input className="form-control input-field" placeholder="Artist" onChange={(event) => setArtistName(event.target.value)} />
+            <input className="form-control input-field" placeholder="Artist Cover Image link" onChange={(event) => setArtistCover(event.target.value)} />
             <input className="form-control input-field" placeholder="Song Title"/>
             {verseField}
-            <button type="button" className="btn verse-btn" onClick={() => setClick([...clicked, clicked[clicked.length - 1] + 1 ])}>New Verse</button>
+            <button type="button" className="btn verse-btn mx-auto" onClick={() => setClick([...clicked, clicked[clicked.length - 1] + 1 ])}>New Verse</button>
+            <button type="submit" className="btn verse-btn mx-auto">Submit</button>
         </form>
     )
 }
