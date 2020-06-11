a3b1cfa64384789a3611ad401f52bdeb437ded30
BOBBY <BOBBYKWONGZW@GMAIL.COM>
2020-06-12 00:54:09 +0800

    editing path in index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use('/graphql', graphqlHTTP({
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.join('build', 'index.html'));
+    res.sendFile(path.join('/client/build', '/index.html'));
   });
 }
 
