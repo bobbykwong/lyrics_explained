95e88045f3793f548d5729ca891d283be59454db
BOBBY <BOBBYKWONGZW@GMAIL.COM>
2020-06-12 01:21:57 +0800

    removed /build from .gitignore

---
+++ client/build/precache-manifest.348506b6adcf9f3ebc7afece9548a40c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b56e22154c2044fc0b428e34223ca699",
+    "url": "/index.html"
+  },
+  {
+    "revision": "645b72988cd6f949d2e7",
+    "url": "/static/css/main.75e513d8.chunk.css"
+  },
+  {
+    "revision": "a6fa6008d1ffc93ae569",
+    "url": "/static/js/2.6d0c5e67.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.6d0c5e67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "645b72988cd6f949d2e7",
+    "url": "/static/js/main.efab233a.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
